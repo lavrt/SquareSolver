0fae4a17d40730ab64f9719e41603fdc70deb36a
lavrt <lavrik-aleksey-1@yandex.ru>
2024-08-16 18:21:19 +0300

    Update main.cpp
    внесены правки в первый вариант

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
-// program for solving quadratic equations
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-void get_num(double *, double *, double *);
-bool input_is_incorrect(double, double, double);
-bool double_equals(double, double, double);
-double solving_linear_equation(double, double);
-void solving_quadratic_equation(double, double, double, double *, double *);
-void solving_if_complex(double, double, double);
+#define EPSILON 1e-8
+
+void get_num(double * ad_a, double * ad_b, double * ad_c);
+bool input_is_incorrect(double coeff_a, double coeff_b, double coeff_c);
+bool double_equals(double num1, double num2);
+double solving_linear_equation(double coeff_b, double coeff_c);
+void solving_quadratic_equation(double coeff_a, double coeff_b, double discriminant, double * ad_x1, double * ad_x2);
 
 int main(void)
 {
-    const double epsilon = 1e-6;
-    double a, b, c;
-    get_num(&a, &b, &c);
-    while (input_is_incorrect(a, b, c)){
+    double coeff_a = 0;
+    double coeff_b = 0;
+    double coeff_c = 0;
+    get_num(&coeff_a, &coeff_b, &coeff_c);
+    while (input_is_incorrect(coeff_a, coeff_b, coeff_c))
+    {
         printf("\n\n\n");
-        get_num(&a, &b, &c);
+        get_num(&coeff_a, &coeff_b, &coeff_c);
     }
     printf("The coefficients are saved.\n");
 
-    if (double_equals(a, 0, epsilon))
+    if (double_equals(coeff_a, 0))
     {
-        printf("\nThe equation is linear.\nAnswer: x = %lf\n", solving_linear_equation(b, c));
+        printf("\nThe equation is linear.\nAnswer: x = %lf\n", solving_linear_equation(coeff_b, coeff_c));
     }
     else
     {
-        double discriminant = b * b - 4 * a * c;
+        double discriminant = coeff_b * coeff_b - 4 * coeff_a * coeff_c;
         double x1, x2;
-        solving_quadratic_equation(a, b, discriminant, &x1, &x2);
+        solving_quadratic_equation(coeff_a, coeff_b, discriminant, &x1, &x2);
 
-        if (double_equals(discriminant, 0, epsilon))
+        if (double_equals(discriminant, 0))
         {
             printf("\nThe equation is quadratic and has two equal solutions.\nAnswer: x1 = %lf, x2 = %lf\n", x1, x2);
         }
@@ -41,15 +43,11 @@ int main(void)
         else if (discriminant < 0)
         {
             printf("\nThere are no solutions in real numbers.\n");
-            solving_if_complex(a, b, discriminant);
-        }
+        }  // add solving in complex numbers later
     }
 
     return 0;
 }
-
-
-
 
 void get_num(double * ad_a, double * ad_b, double * ad_c)
 {
@@ -57,79 +55,65 @@ void get_num(double * ad_a, double * ad_b, double * ad_c)
             "coefficients of the quadratic equation.\n\n");
 
     printf("Enter a: ");
-    while (scanf("%lf", ad_a) != 1){
+    while (scanf("%lf", ad_a) != 1)
+    {
         printf("\nPlease enter the correct numeric value.\nEnter a: ");
         while (getchar() != '\n') continue;
     }
 
     printf("Enter b: ");
-    while (scanf("%lf", ad_b) != 1){
+    while (scanf("%lf", ad_b) != 1)
+    {
         printf("\nPlease enter the correct numeric value.\nEnter b: ");
         while (getchar() != '\n') continue;
     }
 
     printf("Enter c: ");
-    while (scanf("%lf", ad_c) != 1){
+    while (scanf("%lf", ad_c) != 1)
+    {
         printf("\nPlease enter the correct numeric value.\nEnter c: ");
         while (getchar() != '\n') continue;
     }
 }
 
-
-
-
-bool input_is_incorrect(double a, double b, double c)
+bool input_is_incorrect(double coeff_a, double coeff_b, double coeff_c)
 {
     getchar();
 
     printf("\nThe equation you want to solve:\n");
-    printf("%lf*x^2 %+lf*x %+lf = 0\n", a, b, c);
+    printf("%lf*x^2 %+lf*x %+lf = 0\n", coeff_a, coeff_b, coeff_c);
     printf("Enter 'y' if you want to continue or 'n' if you want to change the coefficients: ");
 
     int ch;
-    while ((ch = getchar()) != 'y' && ch != 'n'){
+    while ((ch = getchar()) != 'y' && ch != 'n')
+    {
         printf("Enter 'y' or 'n' without quotes.\n");
         if (ch == '\n') continue;
         while ((getchar()) != '\n') continue;
     }
 
-    return (ch == 'n') ? true : false;
+    return ch == 'n';
 }
 
-
-
-
-double solving_linear_equation(double b, double c)
+bool double_equals(double num1, double num2)
 {
-    return -(b / c);
+    return fabs(num1 - num2) < EPSILON;
 }
 
-
-
-
-void solving_quadratic_equation(double a, double b, double discriminant, double * ad_x1, double * ad_x2)
+double solving_linear_equation(double coeff_b, double coeff_c)
 {
-    if (discriminant > 0)
+    return -(coeff_b / coeff_c);
+}
+
+void solving_quadratic_equation(double coeff_a, double coeff_b, double discriminant, double * ad_x1, double * ad_x2)
+{
+    if (discriminant >= 0)
     {
-        *ad_x1 = (-b - sqrt(discriminant)) / (2 * a);
-        *ad_x2 = (-b + sqrt(discriminant)) / (2 * a);
+        *ad_x1 = (-coeff_b - sqrt(discriminant)) / (2 * coeff_a);
+        *ad_x2 = (-coeff_b + sqrt(discriminant)) / (2 * coeff_a);
     }
-}
-
-
-
-
-bool double_equals(double num1, double num2, double epsilon)
-{
-    return fabs(num1 - num2) < epsilon;
-}
-
-
-
-
-void solving_if_complex(double a, double b, double discriminant)
-{
-    printf("There is a solution in complex numbers.\n");
-    printf("Answer: x1 = %lf%+lf*i, x2 = %lf%+lf*i\n", -b / (2 * a), -sqrt(-discriminant) / (2 * a),
-            -b / (2 * a), sqrt(-discriminant) / (2 * a));
+    else if (discriminant < 0)
+    {
+        // add solving in complex numbers later
+    }
 }
