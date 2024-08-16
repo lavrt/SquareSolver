8937b00af4d4ca60d08ce1621e6cab996edc9e16
lavrt <lavrik-aleksey-1@yandex.ru>
2024-08-17 00:42:55 +0300

    Улучшение интерфейса
    1) Добавил функцию bool program_continue() которая спрашивает в конце программы, хочет ли пользователь продолжить или желает выйти.
    2) добавил для этого в main() цикл do while(program_continue)

--- main.cpp
+++ main.cpp
@@ -8,43 +8,47 @@ bool input_is_incorrect(double coeff_a, double coeff_b, double coeff_c);
 bool double_equals(double num1, double num2);
 double solving_linear_equation(double coeff_b, double coeff_c);
 void solving_quadratic_equation(double coeff_a, double coeff_b, double discriminant, double * ad_x1, double * ad_x2);
+bool program_continue(void);
 
 int main(void)
 {
-    double coeff_a = 0;
-    double coeff_b = 0;
-    double coeff_c = 0;
-    get_num(&coeff_a, &coeff_b, &coeff_c);
-    while (input_is_incorrect(coeff_a, coeff_b, coeff_c))
+    do
     {
-        printf("\n\n\n");
+        double coeff_a = 0;
+        double coeff_b = 0;
+        double coeff_c = 0;
         get_num(&coeff_a, &coeff_b, &coeff_c);
-    }
-    printf("The coefficients are saved.\n");
-
-    if (double_equals(coeff_a, 0))
-    {
-        printf("\nThe equation is linear.\nAnswer: x = %lf\n", solving_linear_equation(coeff_b, coeff_c));
-    }
-    else
-    {
-        double discriminant = coeff_b * coeff_b - 4 * coeff_a * coeff_c;
-        double x1, x2;
-        solving_quadratic_equation(coeff_a, coeff_b, discriminant, &x1, &x2);
-
-        if (double_equals(discriminant, 0))
+        while (input_is_incorrect(coeff_a, coeff_b, coeff_c))
         {
-            printf("\nThe equation is quadratic and has two equal solutions.\nAnswer: x1 = %lf, x2 = %lf\n", x1, x2);
+            printf("\n\n\n");
+            get_num(&coeff_a, &coeff_b, &coeff_c);
         }
-        else if (discriminant > 0)
+        printf("The coefficients are saved.\n");
+
+        if (double_equals(coeff_a, 0))
         {
-            printf("\nThe equation is quadratic and has two different solutions.\nAnswer: x1 = %lf, x2 = %lf\n", x1, x2);
+            printf("\nThe equation is linear.\nAnswer: x = %lf\n", solving_linear_equation(coeff_b, coeff_c));
         }
-        else if (discriminant < 0)
+        else
         {
-            printf("\nThere are no solutions in real numbers.\n");
-        }  // add solving in complex numbers later
-    }
+            double discriminant = coeff_b * coeff_b - 4 * coeff_a * coeff_c;
+            double x1, x2;
+            solving_quadratic_equation(coeff_a, coeff_b, discriminant, &x1, &x2);
+
+            if (double_equals(discriminant, 0))
+            {
+                printf("\nThe equation is quadratic and has two equal solutions.\nAnswer: x1 = %lf, x2 = %lf\n", x1, x2);
+            }
+            else if (discriminant > 0)
+            {
+                printf("\nThe equation is quadratic and has two different solutions.\nAnswer: x1 = %lf, x2 = %lf\n", x1, x2);
+            }
+            else if (discriminant < 0)
+            {
+                printf("\nThere are no solutions in real numbers.\n");
+            }  // add solving in complex numbers later
+        }
+    } while (program_continue());
 
     return 0;
 }
@@ -116,4 +120,21 @@ void solving_quadratic_equation(double coeff_a, double coeff_b, double discrimin
     {
         // add solving in complex numbers later
     }
+}
+
+bool program_continue(void)
+{
+    getchar();
+
+    printf("\n\n\nDo you want to solve the equation again? Enter 'y' if you want to continue or 'q' to exit: ");
+
+    int ch;
+    while ((ch = getchar()) != 'y' && ch != 'q')
+    {
+        printf("Enter 'y' or 'n' without quotes.\n");
+        if (ch == '\n') continue;
+        while (getchar() != '\n') continue;
+    }
+
+    return ch == 'y';
 }
